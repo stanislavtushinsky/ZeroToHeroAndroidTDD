ccb373e2bf4f3fa0c233a2736e93206795b8d658
stanislavtushinsky <stasbjo1212@gmail.com>
2025-06-22 10:45:59 +0300

    ZTH-20 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,22 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.actionButton)
+        val editText = findViewById<TextInputEditText>(R.id.inputEditText)
+        val textView = findViewById<TextView>(R.id.titleTextView)
+
+        button.setOnClickListener {
+            editText.setText(getText(R.string.empty))
+            textView.text = getText(R.string.new_text)
+        }
     }
 }
