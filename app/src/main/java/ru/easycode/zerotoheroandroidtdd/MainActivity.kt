80fd0c9c565ea364ab05657764125fa71239cd02
stanislavtushinsky <stasbjo1212@gmail.com>
2025-06-22 11:36:29 +0300

    ZTH-21 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,50 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textWatcher: TextWatcher
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var savedText = ""
+
+        textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int,
+            ) = Unit
+
+            override fun onTextChanged(
+                s: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int,
+            ) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                binding.actionButton.isEnabled = s?.length == 3
+                savedText = binding.inputEditText.text.toString()
+            }
+        }
+
+        binding.inputEditText.addTextChangedListener(textWatcher)
+
+        binding.actionButton.setOnClickListener {
+            binding.titleTextView.text = savedText
+            binding.inputEditText.setText("")
+            binding.actionButton.isEnabled = false
+        }
     }
 }
